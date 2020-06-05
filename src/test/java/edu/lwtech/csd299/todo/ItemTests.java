c77daeb1eac35030c9de3dc361736688bfdbce69
valentinavolgina2 <s-valentia.volgina@lwtech.edu>
2020-06-05 13:36:03 -0700

    Added Unit Tests for clone() and getListID() methods of Item POJO class

--- src/test/java/edu/lwtech/csd299/todo/ItemTests.java
+++ src/test/java/edu/lwtech/csd299/todo/ItemTests.java
@@ -7,13 +7,13 @@ import static org.junit.Assert.*;
 public class ItemTests {
 
     Item newItem;
-    Item copyItem;
+    Item dbItem;
     Item invalidItem;
 
     @Before
     public void setUp() {
         newItem = new Item("Item #1", 1);
-        copyItem = new Item(10, newItem);
+        dbItem = new Item(10, newItem);
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -39,13 +39,19 @@ public class ItemTests {
     @Test
     public void getIdTest() {
         assertEquals(-1, newItem.getID());
-        assertEquals(10, copyItem.getID());
+        assertEquals(10, dbItem.getID());
     }
 
     @Test
     public void getNameTest() {
         assertEquals("Item #1", newItem.getName());
-        assertEquals("Item #1", copyItem.getName());
+        assertEquals("Item #1", dbItem.getName());
+    }
+
+    @Test
+    public void getListIDTest() {
+        assertEquals(1, newItem.getListID());
+        assertEquals(1, dbItem.getListID());
     }
 
     @Test
@@ -55,19 +61,33 @@ public class ItemTests {
 
     @Test
     public void completeTest() {
-        assertFalse(copyItem.isCompleted());
-        copyItem.complete();
-        assertTrue(copyItem.isCompleted());
+        assertFalse(dbItem.isCompleted());
+        dbItem.complete();
+        assertTrue(dbItem.isCompleted());
     }
 
     @Test
     public void toStringTest() {
-        copyItem.complete();
+        dbItem.complete();
         assertTrue(newItem.toString().startsWith("[-1:"));
         assertTrue(newItem.toString().contains("Item #1"));
         assertTrue(newItem.toString().contains("No"));
         assertTrue(newItem.toString().contains("list id: 1"));
-        assertTrue(copyItem.toString().contains("Yes"));
+        assertTrue(dbItem.toString().contains("Yes"));
+    }
+
+    @Test
+    public void cloneTest() {
+        Item cloneItem = dbItem.clone();
+        dbItem.complete();
+        assertEquals(10, dbItem.getID());
+        assertEquals(10, cloneItem.getID());
+        assertEquals("Item #1", dbItem.getName());
+        assertEquals("Item #1", cloneItem.getName());
+        assertEquals(1, dbItem.getListID());
+        assertEquals(1, cloneItem.getListID());
+        assertTrue(dbItem.isCompleted());
+        assertFalse(cloneItem.isCompleted());
     }
 
 }
