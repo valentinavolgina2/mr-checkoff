3e1186a166716f02a10d72c74c187fb8a5e6bab8
Ryan Aloof <s-ryan.aloof@lwtech.edu>
2020-06-15 12:19:31 -0700

    Added items dao

--- src/main/java/edu/lwtech/csd299/todo/ToDoListServlet.java
+++ src/main/java/edu/lwtech/csd299/todo/ToDoListServlet.java
@@ -19,8 +19,12 @@ public class ToDoListServlet extends HttpServlet {
     private static final String TEMPLATE_DIR = "/WEB-INF/classes/templates";
     private static final Configuration freemarker = new Configuration(Configuration.getVersion());
 
+    private DAO<Member> memberMemoryDao = null;
     private DAO<DemoPojo> demoMemoryDao = null;
     private DAO<ToDoList> todoListDao = null;
+    private DAO<Item> itemMemoryDao = null;
+
+    private static int currentListIndex = 0;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
@@ -44,7 +48,9 @@ public class ToDoListServlet extends HttpServlet {
 
         todoListDao = new ToDoListMemoryDAO();
         todoListDao.insert(new ToDoList("Test list", 1));
-        demoMemoryDao = new DemoPojoMemoryDAO();
+        memberMemoryDao = new MemberMemoryDAO();
+        memberMemoryDao.insert(new Member("1", "1", "Ryan", "A", "f@m.com"));
+        itemMemoryDao = new ItemMemoryDAO();
         addDemoData();
 
         logger.warn("Initialize complete!");
@@ -69,19 +75,28 @@ public class ToDoListServlet extends HttpServlet {
                 model.put("todoLists", todoLists);
                 break;
             case "show":
-                String indexParam = request.getParameter("index");
+
+                String idParam = request.getParameter("listId");
+                int id = (idParam == null) ? 0 : Integer.parseInt(idParam);
+
+                /*String indexParam = request.getParameter("index");
                 int index = (indexParam == null) ? 0 : Integer.parseInt(indexParam);
                 int numItems = demoMemoryDao.getAllIDs().size();
                 int nextIndex = (index + 1) % numItems;
                 int prevIndex = index - 1;
-                if (prevIndex < 0) prevIndex = numItems-1;
-
-                template = "show.ftl";
-                model.put("item", demoMemoryDao.getByIndex(index));
+                if (prevIndex < 0) prevIndex = numItems-1;*/
+               /*model.put("item", demoMemoryDao.getByIndex(index));
                 model.put("prevIndex", prevIndex);
-                model.put("nextIndex", nextIndex);
-                break;
+                model.put("nextIndex", nextIndex);*/
                 
+                ToDoList todoList = todoListDao.getByID(id);
+                List<Item> items = itemMemoryDao.getByOwnerID(id);
+                model.put("todoList", todoList);
+                template = "show.ftl";
+
+                break;
+            case "login":
+                template = "login.ftl";  
             default:
                 logger.debug("Unknown GET command received: " + command);
 
@@ -99,7 +114,122 @@ public class ToDoListServlet extends HttpServlet {
         logger.info("OUT- GET " + request.getRequestURI() + " " + time + "ms");
     }
 
-    //TODO: doPost() goes here - if needed.
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        logger.debug("IN -POST " + request.getRequestURI());
+        long startTime = System.currentTimeMillis();
+        
+        String command = request.getParameter("cmd");
+        if (command == null) command = "";
+
+        int owner = 0;
+        boolean loggedIn = false;
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            try {
+                owner = Integer.parseInt((String)session.getAttribute("owner"));
+                loggedIn = true;
+            } catch (NumberFormatException e) {
+                owner = 0;
+                loggedIn = false;
+            }
+        }
+
+        String message = "";
+        String template = "confirm.ftl";
+        Map<String, Object> model = new HashMap<>();
+        String username = "";
+        String password = "";
+        String firstName = "";
+        String lastName = "";
+        String email = "";
+        Member member;
+        
+        switch (command) {
+
+            case "create":
+                ToDoList newList = getToDoListFromRequest(request, owner);
+
+                if (newList == null) {
+                    logger.info("Create request ignored because one or more fields were empty.");
+                    message = "Your new ToDoList was not created because one or more fields were empty.<br /><a href='?cmd=home'>Home</a>";
+                    break;
+                }
+
+                if (todoListDao.insert(newList) > 0)
+                    message = "Your new ToDoList has been created successfully.<br /><a href='?cmd=home'>Home</a>";
+                else
+                    message = "There was a problem adding your list to the database.<br /><a href='?cmd=home'>Home</a>";
+                break;
+
+            case "login":
+                username = request.getParameter("username");
+                password = request.getParameter("password");
+                
+                
+                member = memberMemoryDao.search("username", username);
+                if (member == null) {
+                    message = "We do not have a member with that username on file. Please try again.<br /><a href='?cmd=login'>Log In</a>";
+                    model.put("loggedIn", loggedIn);
+                    model.put("message", message);
+                    break;
+                }
+
+                if (member.getPassword().equals(password)) {
+                    owner = member.getId();
+                    loggedIn = true;
+
+                    session = request.getSession(true);
+                    session.setAttribute("owner", owner);
+
+                    message = "You have been successfully logged in to your account.<br /><a href='?cmd=show'>Show Lists</a>";
+                } else {
+                    message = "Your password did not match what we have on file.  Please try again.<br /><a href='?cmd=login'>Log In</a>";
+                }
+
+                model.put("loggedIn", loggedIn);
+                model.put("message", message);
+                break;
+
+            case "register":
+                username = request.getParameter("username");
+                password = request.getParameter("password");
+                
+                member = memberMemoryDao.search("username", username);
+                if (member != null) {
+                    message = "That username is already registered here. Please use a different username.<br /><a href='?cmd=login'>Log In</a>";
+                    model.put("message", message);
+                    break;
+                }
+
+                member = new Member(username, password, firstName, lastName, email);
+                memberMemoryDao.insert(member);
+
+                message = "Welcome to TopTopTenLists.com!  You are now a registered member. Please <a href='?cmd=login'>log in</a>.";
+                model.put("message", message);
+                break;
+            case "signup":
+                
+            default:
+                logger.info("Unknown POST command received: " + command);
+
+                // Send 404 error response
+                try {
+                    response.sendError(HttpServletResponse.SC_NOT_FOUND);
+                } catch (IOException e)  {
+                    logger.error("IO Error: ", e);
+                }
+                return;
+        }
+
+        model.put("message", message);
+       
+        processTemplate(response, template, model);
+
+        long time = System.currentTimeMillis() - startTime;
+        logger.info("OUT- GET " + request.getRequestURI() + " " + time + "ms");
+    }
     
     @Override
     public void destroy() {
@@ -111,6 +241,24 @@ public class ToDoListServlet extends HttpServlet {
     @Override
     public String getServletInfo() {
         return "mr-checkoff Servlet";
+    }
+
+
+    private ToDoList getToDoListFromRequest(HttpServletRequest request, int owner) {
+
+        String description = request.getParameter("description");
+        if (description == null) return null;
+
+        List<String> items = new ArrayList<>();
+        for (int i=10; i >= 1; i--) {
+            String item = request.getParameter("item" + i);
+            if (item == null || item.isEmpty())
+                return null;
+            items.add(item);
+        }
+        
+        return null;
+        //return new ToDoList(description, items, owner);
     }
 
     // ========================================================================
